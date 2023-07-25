1efe0cbf0805bdaec6b6e7de9105fee313d3a87d
loctran304 <minhloc3042002@gmail.com>
2023-07-26 01:40:10 +0700

    project

---
+++ accessories.js
@@ -0,0 +1,111 @@
+[
+    {
+        "image":"image/hinh7a.png",
+        "name":"BRITISH RACING GREEN 10 PIECE WATCH BOX WITH STORAGE",
+        "price":1488400
+    },
+    {
+        "image":"image/hinh7b.png",
+        "name":"BRITISH RACING GREEN TRIPLE WATCH ROLL",
+        "price":6697000
+    },
+    {
+        "image":"image/hinh7c.png",
+        "name":"DOUBLE CUB WINDER WITH COVER",
+        "price":17399000
+    },
+    {
+        "image":"image/hinh7f.png",
+        "name":"ROADSTER 10 PIECE WATCH BOX W/ DRAWER",
+        "price":12720000
+    }
+    ,
+    {
+        "image":"image/hinh7e.png",
+        "name":"BRITISH RACING GREEN 6 PIECE WATCH WINDER",
+        "price":91084000
+    }
+    ,
+    {
+        "image":"image/hinh7b.png",
+        "name":"BRITISH RACING GREEN TRIPLE WATCH ROLL",
+        "price":6697000
+    },
+    {
+        "image":"image/hinh7c.png",
+        "name":"DOUBLE CUB WINDER WITH COVER",
+        "price":17399000
+    },
+    {
+        "image":"image/hinh7f.png",
+        "name":"ROADSTER 10 PIECE WATCH BOX W/ DRAWER",
+        "price":12720000
+    }
+    ,
+    {
+        "image":"image/hinh7e.png",
+        "name":"BRITISH RACING GREEN 6 PIECE WATCH WINDER",
+        "price":91084000
+    }
+    ,
+    {
+        "image":"image/hinh7b.png",
+        "name":"BRITISH RACING GREEN TRIPLE WATCH ROLL",
+        "price":6697000
+    },
+    {
+        "image":"image/hinh7c.png",
+        "name":"DOUBLE CUB WINDER WITH COVER",
+        "price":17399000
+    },
+    {
+        "image":"image/hinh7f.png",
+        "name":"ROADSTER 10 PIECE WATCH BOX W/ DRAWER",
+        "price":12720000
+    }
+    ,
+    {
+        "image":"image/hinh7e.png",
+        "name":"BRITISH RACING GREEN 6 PIECE WATCH WINDER",
+        "price":91084000
+    },
+    {
+        "image":"image/hinh7b.png",
+        "name":"BRITISH RACING GREEN TRIPLE WATCH ROLL",
+        "price":6697000
+    },
+    {
+        "image":"image/hinh7c.png",
+        "name":"DOUBLE CUB WINDER WITH COVER",
+        "price":17399000
+    },
+    {
+        "image":"image/hinh7f.png",
+        "name":"ROADSTER 10 PIECE WATCH BOX W/ DRAWER",
+        "price":12720000
+    }
+    ,
+    {
+        "image":"image/hinh7e.png",
+        "name":"BRITISH RACING GREEN 6 PIECE WATCH WINDER",
+        "price":91084000
+    }
+
+    ,
+    {
+        "image":"image/hinh7b.png",
+        "name":"BRITISH RACING GREEN TRIPLE WATCH ROLL",
+        "price":6697000
+    },
+    {
+        "image":"image/hinh7c.png",
+        "name":"DOUBLE CUB WINDER WITH COVER",
+        "price":17399000
+    },
+    {
+        "image":"image/hinh7f.png",
+        "name":"ROADSTER 10 PIECE WATCH BOX W/ DRAWER",
+        "price":12720000
+    }
+    
+]
---
+++ products.js
@@ -0,0 +1,102 @@
+[
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000,
+        "brand":"AUDEMARS PIGUET",
+        "hinh":"image/hinhc1.png",
+        "service":"SELL YOUR WATCH",
+        "hinh8":"image/hinh8a.png"
+    },
+    {
+        "name": "Tudor Pelagos 39",
+        "Image": "image/hinhb2.png",
+        "Price": 123243000,
+        "brand":"BREITLING",
+        "hinh":"image/hinhc2.png",
+        "service":"SERVICE YOU WATCH",
+        "hinh8":"image/hinh8c.png"
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000,
+        "brand":"CARTIER",
+        "hinh":"image/hinhc3.png",
+        "service":"WATCH REFURBISHING",
+        "hinh8":"image/hinh8b.png"
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000,
+        "brand":"IWC",
+        "hinh":"image/hinhc5.png",
+        "service":"VALUATIONS",
+        "hinh8":"image/hinh8d.png"
+    },
+    {
+        "name": "Tudor Pelagos 39",
+        "Image": "image/hinhb2.png",
+        "Price": 123243000,
+        "brand":"PATEK PHILIPPE",
+        "hinh":"image/hinhc4.png"
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000,
+        "brand":"ROLEX",
+        "hinh":"image/hinhc6.png"
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    },{
+        "name": "Tudor Pelagos 39",
+        "Image": "image/hinhb2.png",
+        "Price": 123243000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    }
+    ,{
+        "name": "Tudor Pelagos 39",
+        "Image": "image/hinhb2.png",
+        "Price": 123243000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    },{
+        "name": "Tudor Pelagos 39",
+        "Image": "image/hinhb2.png",
+        "Price": 123243000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    },
+    {
+        "name": "Rolex Datejust 41",
+        "Image": "image/hinhb1.png",
+        "Price": 314262000
+    }
+
+
+]
